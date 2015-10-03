0b6f386e986c04d92abc22f9eaaf118b2ed1d87f
Santiago Callejas <sancagon87@gmail.com>
2015-10-03 13:54:13 -0300

    Se crea Atributo, y metodos en Producto para convertir a JSON

--- src/dominio/Producto.java
+++ src/dominio/Producto.java
@@ -1,16 +1,26 @@
 package dominio;
 
 import java.io.Serializable;
-import java.lang.Long;
-import java.lang.String;
-import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+import com.google.gson.Gson;
 
 /**
  * Entity implementation class for Entity: Producto
  *
  */
 @Entity
-
+@Access(AccessType.FIELD)
 public class Producto implements Serializable {
 
 	   
@@ -21,39 +31,68 @@ public class Producto implements Serializable {
 	private double precio;
 	@ManyToOne
 	private Categoria categoria;
-	// agregar imagen
+	// TODO agregar imagen
+	@Transient
+	private List<Atributo> atributosList;
+
 	private static final long serialVersionUID = 1L;
 
 	public Producto() {
 		super();
-	}   
+	}	
 	public Long getIdProducto() {
 		return this.idProducto;
-	}
-
+	}	
 	public void setIdProducto(Long idProducto) {
 		this.idProducto = idProducto;
 	}   
 	public String getNombre() {
 		return this.nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}   
 	public String getDescripcion() {
 		return this.descripcion;
 	}
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}   
 	public double getPrecio() {
 		return this.precio;
 	}
-
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-   
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	//PARA CONVERTIR A Y DESDE JSON - LO USA EL JPA, NO NOSOTROS!
+	@Access(AccessType.PROPERTY)	
+	@Column(name="atributos", nullable = false)
+	public String getAtributos() {
+		Gson gson = new Gson();
+		return gson.toJson(atributosList);
+	}
+	public void setAtributos(String attrs) {
+		if( !attrs.equals("null") ) {
+			Gson gson = new Gson();
+			Atributo[] auxArray = gson.fromJson(attrs, Atributo[].class);
+			List<Atributo> auxList = new ArrayList<>(Arrays.asList(auxArray));
+			this.atributosList = auxList;
+		}
+	}
+	
+	public List<Atributo> getAtributosList() {
+		return atributosList;
+	}
+	public void setAtributosList(List<Atributo> atributosList) {
+		this.atributosList = atributosList;
+	}
+		
+	
 }
