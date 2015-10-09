bb469737aabed621a66e5c9ab55ea23ffc4625bd
noeliaGonzalez <noeggamarra@gmail.com>
2015-10-09 20:47:30 -0300

    DataAV DataCategoria CAV

--- src/negocio/ControladorAV.java
+++ src/negocio/ControladorAV.java
@@ -1,6 +1,10 @@
 package negocio;
 
 import javax.ejb.Stateless;
+import dominio.AV;
+import dominio.Usuario;
+import dominio.datatype.DataAV;
+
 
 /**
  * Session Bean implementation class ControladorAV
@@ -8,11 +12,25 @@ import javax.ejb.Stateless;
 @Stateless
 public class ControladorAV implements IControladorAV {
 
-    /**
-     * Default constructor. 
-     */
-    public ControladorAV() {
-        // TODO Auto-generated constructor stub
-    }
+	@Override
+	public DataAV altaAV(String nombreAV, Usuario usuarioCreador) {
+		DataAV av= new DataAV(nombreAV,usuarioCreador);
+	    return av;
+		
+	}
+	public boolean existeAVusuario(String nombreAV, DataUsuario usuarioCreador){
+		return true;
+		
+	}
+	//datos de estilo si se modifican
+	public void modificarAV(String nombreAV, String nuevoNombreAV){
+		
+		
+	}
+	public void eliminarAV(String nombreAV, DataUsuario usuarioCreador){
+		
+	}
+
+
 
 }
--- src/negocio/IControladorAV.java
+++ src/negocio/IControladorAV.java
@@ -2,7 +2,17 @@ package negocio;
 
 import javax.ejb.Local;
 
+import dominio.datatype.DataAV;
+import dominio.datatype.DataUsuario;
+
 @Local
 public interface IControladorAV {
-
+	
+	//un usu no puede tener 2AV mismo nombre
+	public boolean existeAVusuario(String nombreAV, DataUsuario usuarioCreador);
+	public DataAV altaAV(String nombreAV, DataUsuario usuarioCreador);
+	public void modificarAV(String nombreAV, String nuevoNombreAV);//datos de estilo si se modifican
+	public void eliminarAV(String nombreAV, DataUsuario usuarioCreador);
+	
+	
 }
